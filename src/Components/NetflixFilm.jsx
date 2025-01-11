d6b47ee4e7ce750b9c0127d4aff019ec64c04651
Rachele Barberis <rachele.barberis.12@gmail.com>
2025-01-11 15:28:09 +0100

    commit con extra1

--- src/Components/NetflixFilm.jsx
+++ src/Components/NetflixFilm.jsx
@@ -1,34 +1,41 @@
 /* eslint-disable react/prop-types */
 import { Component } from "react";
-import { Container, Row, Col, Modal } from "react-bootstrap";
+import { Container, Row, Col, Spinner, Alert } from "react-bootstrap";
 
 const URL = "https://www.omdbapi.com/?apikey=90251c4f&s=";
 
 class NetflixFilm extends Component {
   state = {
     search: [],
-    showModal: false,
-    selectedFilm: null,
+    isLoading: true,
+    isError: false,
   };
 
   functionFilm = async (searchKey) => {
     try {
+      this.setState({ isLoading: true, isError: false });
       const response = await fetch(URL + searchKey);
       if (response.ok) {
         const data = await response.json();
-        const show = data.Search.slice(0, 6);
         if (data.Search) {
+          const show = data.Search.slice(0, 6);
           this.setState({
             search: show,
+            isLoading: false,
           });
         } else {
           console.log("Nessun risultato trovato.");
+          this.setState({ isLoading: false });
         }
       } else {
         throw new Error("Errore nel recupero dati API");
       }
     } catch (error) {
-      console.log(error);
+      console.error("Errore:", error);
+      this.setState({
+        isLoading: false,
+        isError: true,
+      });
     }
   };
 
@@ -37,59 +44,39 @@ class NetflixFilm extends Component {
     this.functionFilm(searchKey);
   }
 
-  openModal = (film) => {
-    this.setState({
-      showModal: true,
-      selectedFilm: film,
-    });
-  };
-
-  closeModal = () => {
-    this.setState({
-      showModal: false,
-      selectedFilm: null,
-    });
-  };
-
   render() {
-    const { showModal, selectedFilm } = this.state;
+    const { search, isLoading, isError } = this.state;
 
     return (
-      <>
-        <Container fluid={true} className="bg-dark">
-          <Row className="justify-content-center">
-            <h2 className="text-secondary">Guarda Ora</h2>
-            {this.state.search.map((film) => (
-              <Col sm={4} lg={2} key={film.imdbID} className="mb-3 p-1">
-                <img
-                  src={film.Poster}
-                  alt={film.Title}
-                  className="img-fluid h-100"
-                  onClick={() => this.openModal(film)}
-                  style={{ cursor: "pointer" }}
-                />
-              </Col>
-            ))}
-          </Row>
-        </Container>
-
-        {this.state.selectedFilm && (
-          <Modal show={showModal} onHide={this.closeModal} centered size="sm">
-            <Modal.Header closeButton>
-              <Modal.Title className="text-center text-secondary">
-                {selectedFilm.Title}
-              </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <img
-                src={selectedFilm.Poster}
-                alt={selectedFilm.Title}
-                className="img-fluid mb-3"
-              />
-            </Modal.Body>
-          </Modal>
-        )}
-      </>
+      <Container fluid={true} className="bg-dark text-white py-4">
+        <Row className="justify-content-center">
+          <h2 className="text-secondary">Guarda Ora</h2>
+          {isError && (
+            <Alert variant="danger" className="w-100 text-center">
+              Si è verificato un errore nel caricamento dei dati.
+            </Alert>
+          )}
+          {isLoading ? (
+            <div className="text-center my-4 w-100">
+              <Spinner animation="border" variant="light" />
+              <p>Caricamento in corso...</p>
+            </div>
+          ) : (
+            <Row className="justify-content-center">
+              {search.map((film) => (
+                <Col sm={4} lg={2} key={film.imdbID} className="mb-3 p-1">
+                  <img
+                    src={film.Poster}
+                    alt={film.Title}
+                    className="img-fluid h-100"
+                  />
+                  <p className="text-center mt-2">{film.Title}</p>
+                </Col>
+              ))}
+            </Row>
+          )}
+        </Row>
+      </Container>
     );
   }
 }
