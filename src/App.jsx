2945b047956b63d2c71d94b5162e223788da6983
Rachele Barberis <rachele.barberis.12@gmail.com>
2025-01-11 12:31:57 +0100

    finito senza extra

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import NetflixNav from "./Components/NetflixNav";
 import NetflixFooter from "./Components/NetflixFooter";
 import NetflixHeader from "./Components/NetflixHeader";
+import NetflixFilm from "./Components/NetflixFilm";
 
 function App() {
   return (
     <>
       <NetflixNav />
       <NetflixHeader />
+      <NetflixFilm searchKey="Avengers" />
+      <NetflixFilm searchKey="Indiana Jones" />
+      <NetflixFilm searchKey="Harry Potter" />
       <NetflixFooter />
     </>
   );
--- src/Components/NetflixFilm.jsx
+++ src/Components/NetflixFilm.jsx
@@ -1,9 +1,54 @@
+/* eslint-disable react/prop-types */
 import { Component } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+const URL = "https://www.omdbapi.com/?apikey=90251c4f&s=";
 
 class NetflixFilm extends Component {
-    render() {
-        return(
-            
-        )
+  state = {
+    search: [],
+  };
+
+  functionFilm = async (searchKey) => {
+    try {
+      const response = await fetch(URL + searchKey);
+      if (response.ok) {
+        const data = await response.json();
+        const show = data.Search.slice(0, 6);
+        if (data.Search) {
+          this.setState({
+            search: show,
+          });
+        } else {
+          console.log("Nessun risultato trovato.");
+        }
+      } else {
+        throw new Error("Errore nel recupero dati API");
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
+
+  componentDidMount() {
+    const { searchKey } = this.props;
+    this.functionFilm(searchKey);
+  }
+
+  render() {
+    return (
+      <Container fluid={true} className="bg-dark">
+        <Row className="justify-content-center">
+          <h2 className="text-secondary">Guarda Ora</h2>
+          {this.state.search.map((film) => (
+            <Col sm={4} lg={2} key={film.imdbID} className="mb-4">
+              <img src={film.Poster} alt={film.Title} className="img-fluid" />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    );
+  }
 }
+
+export default NetflixFilm;
